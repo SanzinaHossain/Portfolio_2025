1483b9d6704b19ee50050e2f6cfaf29e08837087
SanzinaHossain <sanzina67hossain@gmail.com>
2025-07-17 16:39:14 +0600

    about me first section created

---
+++ src/components/Screen/About/Data.jsx
@@ -0,0 +1,14 @@
+export const techData = [
+  {
+    name: "🛠️ Tech Setup",
+    description:
+      "Wondering what I use for development? Here's a look at the tools,apps, and gear I rely on every day.",
+    button_name: "Explore Setup",
+  },
+  {
+    name: "📚 My Blogs",
+    description:
+      "Here’s a quick look at the tools, apps, and gear I rely on daily to code, create, and stay productive.",
+    button_name: "Explore Blogs",
+  },
+];
